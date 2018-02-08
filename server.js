ab145b947294de5c624aa4b29223794b27116eb8
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2018-02-08 10:40:13 +0400

    a

---
+++ server.js
@@ -0,0 +1,19 @@
+// modules
+var static = require( 'node-static' ),
+    port = 8080,
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './public', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen(process.env.PORT)
+
+//.listen(process.env.PORT)
